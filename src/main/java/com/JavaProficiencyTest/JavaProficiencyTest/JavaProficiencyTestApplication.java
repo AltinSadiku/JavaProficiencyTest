f335e3c6c50320d6f774220c293f322894c600a7
AltinSadiku <sadikualtin97@gmail.com>
2021-05-09 01:44:02 +0200

    Initial Commit!

---
+++ src/main/java/com/JavaProficiencyTest/JavaProficiencyTest/JavaProficiencyTestApplication.java
@@ -0,0 +1,13 @@
+package com.JavaProficiencyTest.JavaProficiencyTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProficiencyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProficiencyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaProficiencyTest/JavaProficiencyTest/JavaProficiencyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaProficiencyTest.JavaProficiencyTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProficiencyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
